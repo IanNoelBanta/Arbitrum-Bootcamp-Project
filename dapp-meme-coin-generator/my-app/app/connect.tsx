84f4793b737a8799e53c3dacd3d100b8dec47d37
Ian Noel Banta <81598611+IanNoelBanta@users.noreply.github.com>
2024-02-20 07:06:49 +0000

    connect component

---
+++ dapp-meme-coin-generator/my-app/app/connect.tsx
@@ -0,0 +1,13 @@
+export default function walletConnection (isConnected: boolean) {
+    var connectionStatus = "Connect your Wallet";
+
+    if (isConnected) {
+        connectionStatus = "Wallet Connected!";
+    }
+    
+    return (
+        <main className="mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500">
+            <h1 className="text-4xl font-bold">{connectionStatus}</h1>
+        </main>
+    );    
+}
