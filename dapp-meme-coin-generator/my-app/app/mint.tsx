d90d0bb415b611359bc9dff5eb322e12ff043448
Ian Noel Banta <81598611+IanNoelBanta@users.noreply.github.com>
2024-02-20 09:04:32 +0000

    working input field - address and amount

--- dapp-meme-coin-generator/my-app/app/mint.tsx
+++ dapp-meme-coin-generator/my-app/app/mint.tsx
@@ -1,28 +1,25 @@
-import { useEffect, useState } from "react";
+function StartMinting(address: string, changeAdd: Function, value: number, handleChange: Function) {
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <input
+        type="text"
+        className="border rounded-md p-3 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:border-transparent"
+        value={address}
+        onChange={(e) => changeAdd(e)}
+        placeholder="Enter wallet address"
+        style={{ color: "black" }}
+      />
 
-function StartMinting() {
-    const [value, setValue] = useState<number>(0);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const inputValue = e.target.value;
-        // Check if the input value is a valid number
-        if (!isNaN(Number(inputValue))) {
-            setValue(Number(inputValue));
-        } else {
-            // If the input is not a valid number, set the value to an empty string
-            setValue(0);
-        }
-    };
-
-    return (
-        <input
-            type="text"
-            className="border rounded-md p-2"
-            value={value}
-            onChange={(e) => Number(e.target.value)}
-            placeholder="Enter a number"
-        />
-    );
+      <input
+        type="number"
+        className="border rounded-md p-3 focus:outline-none focus:ring-4 focus:ring-pink-500 focus:border-transparent"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        placeholder="Enter amount to mint"
+        style={{ color: "black" }}
+      />
+    </div>
+  );
 }
 
 export default StartMinting;
