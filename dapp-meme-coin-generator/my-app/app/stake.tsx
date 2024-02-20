fb5955348303b9b1d49f884196d1c918eb39c424
Ian Noel Banta <81598611+IanNoelBanta@users.noreply.github.com>
2024-02-20 15:23:53 +0000

    created stake component

---
+++ dapp-meme-coin-generator/my-app/app/stake.tsx
@@ -0,0 +1,25 @@
+function StartStaking(address: string, changeAdd: Function, value: number, handleChange: Function) {
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <input
+        type="text"
+        className="border rounded-md p-3 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:border-transparent"
+        value={address}
+        onChange={(e) => changeAdd(e)}
+        placeholder="Enter wallet address"
+        style={{ color: "black" }}
+      />
+
+      <input
+        type="number"
+        className="border rounded-md p-3 focus:outline-none focus:ring-4 focus:ring-pink-500 focus:border-transparent"
+        value={value}
+        onChange={(e) => handleChange(e)}
+        placeholder="Enter amount to mint"
+        style={{ color: "black" }}
+      />
+    </div>
+  );
+}
+
+export default StartStaking;
