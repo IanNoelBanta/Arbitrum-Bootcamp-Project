6ffc99bd3e48b6d9dfc775bb458937eb4d5f7ab1
Ian Noel Banta <81598611+IanNoelBanta@users.noreply.github.com>
2024-02-20 15:23:33 +0000

    display adjustments + cards

--- dapp-meme-coin-generator/my-app/app/page.tsx
+++ dapp-meme-coin-generator/my-app/app/page.tsx
@@ -7,23 +7,61 @@ import { getContract } from "../config";
 import Image from "next/image";
 import walletConnection from "./connect";
 import startMinting from "./mint";
-/*----------------------------------------------------------------- */
+import StartStaking from "./stake";
 
+/*----------------------------------------------------------------- */
 
 /*-------------------------STATES----------------------- */
 
 export default function Home() {
   const [walletKey, setwalletKey] = useState("");
-  const [currentData, setcurrentData] = useState("");
   const [isWalletConnected, setisWalletConnected] = useState(false);
   const [mintingAmount, setMintingAmount] = useState<number>();
   const [mintingAddress, setMintingAddress] = useState("");
+  const [message, setMessage] = useState("");
+  const [chosenButton, setChosenButton] = useState<number>();
 
-/*----------------------------------------------------------------- */
+  /*----------------------------------------------------------------- */
+  const showCard = () => {
+    switch (chosenButton) {
+      case 0:
+        connectWallet();
+        return (
+          <div className="mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500">
+            <p>{message}</p>
+          </div>
+        );
+      case 1:
+        return startMinting(
+          mintingAddress,
+          addressChange,
+          Number(mintingAmount),
+          amountChange
+        );
+      // case 2:
+      //   return (
+      //     <div className="bg-green-500 p-4 rounded-lg text-white">
+      //       <p>Coins Withdrawn!</p>
+      //     </div>
+      //   );
+      // case 3:
+      //   return (
+      //     <div className="bg-green-500 p-4 rounded-lg text-white">
+      //       <p>Coins Withdrawn!</p>
+      //     </div>
+      //   );
+      default:
+        return (
+          <div className="mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500">
+            <p>Start by connecting your wallet</p>
+          </div>
+        );
+    }
+  };
 
-/*-------------------------WALLET CONNECTION----------------------- */
+  /*-------------------------WALLET CONNECTION----------------------- */
   const connectWallet = async () => {
-    if (isWalletConnected){
+    if (isWalletConnected) {
       return;
     }
 
@@ -33,10 +71,10 @@ export default function Home() {
     });
     setwalletKey(accounts[0]);
     setisWalletConnected(true);
-    setcurrentData("Wallet Connected!");
+    setMessage("Wallet Connected!");
     console.log("CONNECTED!");
   };
-/*-------------------------MINTING----------------------- */
+  /*-------------------------MINTING----------------------- */
   const mintCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -46,7 +84,6 @@ export default function Home() {
       const tx = await contract.mint(walletKey, mintingAmount);
       await tx.wait();
       console.log(tx);
-      // setcurrentData(mintingAmount + " Coins Minted!");
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
       alert(`Minting failed: ${decodedError?.args}`);
@@ -68,7 +105,7 @@ export default function Home() {
     setMintingAddress(inputValue);
     console.log(inputValue);
   };
-/*----------------------------------------------------------------- */
+  /*----------------------------------------------------------------- */
 
   const stakeCoin = async () => {
     const { ethereum } = window as any;
@@ -79,7 +116,7 @@ export default function Home() {
       const tx = await contract.stake(walletKey, 1);
       console.log(tx);
       await tx.wait();
-      setcurrentData("Coins Staked!");
+      setMessage("Coins Staked!");
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
       alert(`Staking failed: ${decodedError?.args}`);
@@ -95,155 +132,131 @@ export default function Home() {
       const tx = await contract.withdraw(walletKey);
       console.log(tx);
       await tx.wait();
-      setcurrentData("Coins Withdrawn!");
+      setMessage("Coins Withdrawn!");
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
       alert(`Withdrawal failed: ${decodedError?.args}`);
     }
   };
-  
-
-  // ------------------------------SAMPLE FUNCTIONS--------------------------------
-  const setData = async () => {
-    const { ethereum } = window as any;
-    const provider = new BrowserProvider(ethereum);
-    const signer = await provider.getSigner();
-    const contract = getContract(signer);
-    try {
-      const tx = await contract.setData("Hello Baby");
-      await tx.wait();
-    } catch (e: any) {
-      const decodedError = contract.interface.parseError(e.data);
-      alert(`Transaction failed: ${decodedError?.args}`);
-    }
-  };
-
-  const getData = async () => {
-    const { ethereum } = window as any;
-    const provider = new BrowserProvider(ethereum);
-    const signer = await provider.getSigner();
-    const contract = getContract(signer);
-    try {
-      const tx = await contract.data();
-      alert(tx);
-
-      setcurrentData(tx);
-    } catch (e: any) {
-      const decodedError = contract.interface.parseError(e.data);
-      alert(`Transaction failed: ${decodedError?.args}`);
-    }
-  };
-  // --------------------------------------------------------------------------------
-
-
-
-
-
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-clip-text bg-gradient-to-b from-blue-400 to-blue-600 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Ian Noel M. Banta - ABC04
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-br from-pink-900 to-blue-900 overflow-hidden">
+      <div className="flex-col items-center justify-between rounded-lg px-5 py-5 bg-gray-900 transition-all duration-300 hover:shadow-lg hover:border-transparent hover:bg-transparent hover:dark:border-neutral-700 hover:dark:bg-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-gray-500 flex-grow">
+        <div className="flex flex-col items-center justify-center w-full h-full">
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-40">
+            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-clip-text bg-gradient-to-b from-blue-400 to-blue-600 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+              Ian Noel M. Banta - ABC04
+            </p>
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+              <a
+                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                By{" "}
+                <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  className="dark:invert"
+                  width={100}
+                  height={24}
+                  priority
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className="">
+            {showCard()}
+            {/* {startMinting(
+            mintingAddress,
+            addressChange,
+            Number(mintingAmount),
+            amountChange
+          )} */}
+          </div>
+
+          <div></div>
+
+          <div className="mt-20 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            <a
+              className="group rounded-lg border border-transparent px-5 py-4 transition-all duration-300 hover:shadow-lg hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => setChosenButton(0)}
+            >
+              <h2
+                className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}
+              >
+                Connect your Wallet{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Connect your metamask wallet.
+              </p>
+            </a>
+
+            <a
+              className="group rounded-lg border border-transparent px-5 py-4 transition-all duration-300 hover:shadow-lg hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => setChosenButton(1)}
+            >
+              <h2
+                className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}
+              >
+                Mint Coins{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Start Generating Coins!
+              </p>
+            </a>
+
+            <a
+              className="group rounded-lg border border-transparent px-5 py-4 transition-all duration-300 hover:shadow-lg hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={stakeCoin}
+            >
+              <h2
+                className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}
+              >
+                Stake Coins{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                Hold and earn rewards!
+              </p>
+            </a>
+
+            <a
+              className="group rounded-lg border border-transparent px-5 py-4 transition-all duration-300 hover:shadow-lg hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={withdrawCoin}
+            >
+              <h2
+                className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}
+              >
+                Withdraw Coins{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+                Go home and take your coins!
+              </p>
+            </a>
+          </div>
         </div>
-      </div>
-      
-      <div className="">
-        {startMinting(mintingAddress, addressChange, Number(mintingAmount), amountChange)}
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          // href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={connectWallet}
-        >
-          {/* mb-3 text-2xl font-semibold */}
-          <h2 className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}>
-            Connect your Wallet{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Connect your metamask wallet.
-          </p>
-        </a>
-
-        <a
-          // href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={mintCoin}
-        >
-          <h2 className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}>
-            Mint Coins{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Start Generating Coins!
-          </p>
-        </a>
-
-        <a
-          // href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={stakeCoin}
-        >
-          <h2 className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}>
-            Stake Coins{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Hold and earn rewards!
-          </p>
-        </a>
-
-        <a
-          // href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={withdrawCoin}
-        >
-          <h2 className={`mb-3 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500`}>
-            Withdraw Coins{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Go home and take your coins!
-          </p>
-        </a>
       </div>
     </main>
   );
